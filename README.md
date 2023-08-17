92a5975b2af604627b82c5dfc0f89517dfc48ce8
donid <donid@users.noreply.github.com>
2023-08-17 10:23:35 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MDnsHostAnnouncer
-MDnsHostAnnouncer allow to create alias names for hosts in a local network
+MDnsHostAnnouncer allows you to create alias names for hosts in a local network
+
+This tool is currently only tested with a FritzBox (Firmware 7.29) as DNS server.
+
+I haven't found a way to create an alias (or CNAME) for a network device (PC, notebook etc.).
+in the FritzBox web-ui for DNS. But I saw a comment in an Internet forum, where someone
+claimed that the FritzBox listens to mDNS (MultiCastDNS) announcements and "integrates" them into its own list of hosts.
+
+Example usage:
+If you have a physical machine named 'AlwaysOnSrv' an run the following command on it:
+
+*MDnsHostAnnouncer DatabaseSrv*
+
+You can now use the name 'DatabaseSrv' instead of 'AlwaysOnSrv' in ping commands or
+http requests, or anywhere else.
+
+I couldn't find detailed documentation for this behavior, only a statement from AVM that
+"the Fritz!Box supports mDNS, but not Bonjour".
+
+According to my experiments the FritzBox will "forget" the "mDNS entries" in
+its host list after about 6 hours.
---
+++ src/MDnsHostAnnouncer/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+using Makaretu.Dns;
+
+namespace MDnsHostAnnouncer
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				Console.WriteLine("Usage:");
+				Console.WriteLine("> MDnsHostAnnouncer AliasHostName");
+				Console.WriteLine("Creates an alias 'AliasHostName' for the current IPAddress of the current host.");
+				Console.WriteLine("> MDnsHostAnnouncer AliasHostName 192.168.178.42 192.168.178.43");
+				Console.WriteLine("Creates an alias 'AliasHostName' for the given IPAddresses.");
+				return;
+			}
+
+			string alias = args[0];
+			if (!IsValidHostName(alias))
+			{
+				Console.WriteLine($"AliasHostName ({alias}) is not valid ( use 2-63 chars: a-z, A-Z, 0-9 or '-' )");
+				return;
+			}
+
+			List<IPAddress> ipAddresses = new();
+
+			if (args.Length > 1)
+			{
+				for (int index = 1; index < args.Length; index++)
+				{
+					string currentArg = args[index];
+					IPAddress? iPAddress = ConvertToIpAddress(currentArg);
+					if (iPAddress == null)
+					{
+						Console.WriteLine($"({index}) '{currentArg}' is not a valid IPAddress.");
+						return;
+					}
+					ipAddresses.Add(iPAddress);
+				}
+			}
+
+			Console.WriteLine($"Creating alias: '{alias}'");
+			if (ipAddresses.Any())
+			{
+				Console.WriteLine($" for IP(s)");
+				foreach (IPAddress iPAddress in ipAddresses)
+				{
+					Console.WriteLine("  " + iPAddress.ToString());
+				}
+			}
+			else
+			{
+				Console.WriteLine($" for current host");
+			}
+
+			List<IPAddress>? nullableIpAddresses = ipAddresses.Any() ? ipAddresses : null;
+			var service = new ServiceProfile("", alias, 0, nullableIpAddresses);
+			var sd = new ServiceDiscovery();
+			sd.Announce(service);
+		}
+
+		private static IPAddress? ConvertToIpAddress(string currentArg)
+		{
+			IPAddress.TryParse(currentArg, out IPAddress? result);
+			return result;
+		}
+
+		// this is a simplified check - not all DNS implementations have the same requirements
+		private static bool IsValidHostName(string alias)
+		{
+			if (Regex.Match(alias, "^[A-Z0-9\\-]*$", RegexOptions.IgnoreCase).Success == false)
+			{
+				return false;
+			}
+			if (alias.Length < 2 || alias.Length > 63)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+}
